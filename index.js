c39cc1a927ba5b25a8ac38f890838aa46e300fd6
gobetoslimmy <emmanuelngesa0@gmail.com>
2026-06-19 10:19:52 +0300

    addition of js statements

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+// function to calculate 10% tax on a given amount
+function calculateTax(amount, taxrate) {
+  return amount * taxrate;
+}
 
+//convert a string to uppercase
+function convertToUppercase(str) {
+  return str.toUpperCase();
+}
 
+// function to find the maximum number in an array
+function findMaximum(num1, num2) {
+  return Math.max(num1, num2);
+}
 
+// function to check if a string is a palindrome
+function isPalindrome(word) {
+  const reversed = word.split("").reverse().join("");
+  return (word, toLowerCase() === reversed.toLowerCase());
+}
 
+// function to calculate discounted price
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = originalPrice * (discountPercentage / 100);
+  return originalPrice - dicountAmount;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// This is required for the test to function properly
+module.exports = {
+  calculateTax,
+  convertToUppercase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
